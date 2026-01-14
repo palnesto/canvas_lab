90dcfbef293625b5413f02398c9535a8cde18472
VIDHI-tech <gaydhanevidhi@gmail.com>
2026-01-14 21:28:33 +0530

    geo and amy

--- src/App.tsx
+++ src/App.tsx
@@ -160,6 +160,7 @@ export default function App() {
           <div className="flex flex-col md:flex-row justify-center items-center gap-10 px-3 lg:px-10">
             <a
               href="/amy.pdf"
+              target="_blank"
               className="relative block w-full md:w-1/2 h-60 lg:h-100 rounded-4xl overflow-hidden shadow-lg"
             >
               <img
